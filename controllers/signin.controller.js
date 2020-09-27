b7f08811a10536320e30b3b4e4e17fe29edcb489
Micah Elijah <melijah200@gmail.com>
2020-09-27 20:00:45 +0100

    fixed signin and other bugs

--- controllers/signin.controller.js
+++ controllers/signin.controller.js
@@ -3,13 +3,57 @@ const bcrypt = require("bcryptjs");
 
 exports.signin = async(req, res)=>{
     let { email, password } = req.body;
-    await database.query(`select * from users where email = '${email}'`, async (error, result, fields)=>{
+
+    if(!email || !password){
+        return res.status(400).json({
+            success: false,
+            message: "operation unsuccessful",
+            error: {
+                statusCode: 400,
+                description: "email or password cannot be empty"
+            }
+        });
+    }
+
+    await database.query(`SELECT * FROM users WHERE email = '${email}'`, async (error, result, fields)=>{
         if(error){
-            return res.send(error);
+            return res.status(401).json({
+                success: false,
+                message: "operation unsuccessful",
+                error: {
+                    statusCode: 401,
+                    description: "incorrect password"
+                }
+            });
         }
-        if(await bcrypt.compare(password, result[0].password)){
-             return res.send(result);
+        if(result.length){
+            if (await bcrypt.compare(password, result[0].password)) {
+              return res.status(200).json({
+                success: true,
+                message: "operation successful",
+                data: {
+                  statusCode: 200,
+                  description: "login successful",
+                  user: result[0],
+                },
+              });
+            }
+            return res.status(404).json({
+                success: false,
+                message: "operation unsuccessful",
+                error: {
+                    statusCode: 404,
+                    description: "invalid credentials"
+                }
+            });
         }
-        return res.send("incorrect password");
+        return res.status(404).json({
+            success: false,
+            message: "operation unsuccessful",
+            error: {
+                statusCode: 404,
+                description: "invalid credentials"
+            }
+        });
     });
 };
--- controllers/signup.controller.js
+++ controllers/signup.controller.js
@@ -5,6 +5,23 @@ const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = process.env;
 
 exports.signup = async(req, res)=>{
+
+    let {
+        email,
+        password,
+    } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+            success: false,
+            message: "bad request",
+            error: {
+                statusCode: 400,
+                description: "email or password cannot be blank"
+            }
+        });
+    }
+
     await database.query(
         `CREATE TABLE IF NOT EXISTS accounts(
             acc_no TEXT(10), 
@@ -12,74 +29,97 @@ exports.signup = async(req, res)=>{
             acc_bank TEXT,
             acc_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL
         )`,
-        (error, result, fields)=>{
-            if(error) return console.log(error);
-            console.log("success1");
-        }
-    );
-
-    await database.query(
-      `CREATE TABLE IF NOT EXISTS users(
-          user_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, 
-          email TEXT, 
-          firstname TEXT, 
-          lastname TEXT, 
-          password TEXT, 
-          account INT,
-          token TEXT,
-          createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-          FOREIGN KEY (account)references accounts(acc_id)
-        )`,
-      (error, result, fields) => {
-        if (error) {
-          return console.log(error);
-        }
-        console.log("success2");
-      }
-    );
-    let { email, password, acc_name, acc_no, acc_bank } = req.body;
-   if(!email || !password){
-       return res.status(400).json({
-        success: false,
-        message: "bad request",
-        error: {
-            statusCode: 400,
-            description: "email or password cannot be blank"
-        }
-       });
-   }
-   password = await bcrypt.hash(password, 10);
-   let token = await jwt.sign({
-       email,
-   }, JWT_SECRET);
-
-   database.query("INSERT INTO users(email, password, token) VALUES(?, ?, ?)", [email, password, token], (error, result, fields)=>{
-    if(error){
-        console.log(error);
-        return res.send(error);
-    }
-    console.log(result);
-    return res.status(201).json({
-        success: true,
-        message: "operation successful",
-        data: {
-            statusCode: 201,
-            user: {
-                user_id: result.insertId,
-                email,
-                password,
-                token
+        async (error, result, fields)=>{
+            if(error){
+                return res.status(500).json({
+                    success: false,
+                    message: "operation unsuccessful",
+                    error: {
+                        statusCode: 500,
+                        description: "something went wrong"
+                    }
+                });
             }
+            database.query(
+                `CREATE TABLE IF NOT EXISTS users(
+                    user_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+                    email TEXT, 
+                    firstname TEXT, 
+                    lastname TEXT, 
+                    password TEXT, 
+                    account INT,
+                    token TEXT,
+                    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    FOREIGN KEY (account)references accounts(acc_id)
+                )`,
+                async(error, result, fields) => {
+                    if (error) {
+                        return res.status(500).json({
+                            success: false,
+                            message: "operation unsuccessful",
+                            error: {
+                                statusCode: 500,
+                                description: "something went wrong"
+                            }
+                        });
+                    }
+                   password = await bcrypt.hash(password, 10);
+                   let token = await jwt.sign({
+                       email,
+                   }, JWT_SECRET);
+
+                   database.query(`SELECT * FROM users WHERE email = '${email}'`, (error, result) => {
+                       if (error) {
+                           return res.status(500).json({
+                               success: false,
+                               message: "operation unsuccessful",
+                               error: {
+                                   statusCode: 500,
+                                   description: "something went wrong"
+                               }
+                           });
+                       }
+                       if (result.length) {
+                           return res.status(409).json({
+                               success: false,
+                               message: "operation unsuccessful",
+                               error: {
+                                   statusCode: 409,
+                                   description: "user already exists"
+                               }
+                           });
+                       }
+
+                       database.query("INSERT INTO users(email, password, token) VALUES(?, ?, ?)", [email, password, token], (error, result, fields) => {
+                           if (error) {
+                               console.log(error);
+                               return res.status(500).json({
+                                   success: false,
+                                   message: "operation unsuccessful",
+                                   error: {
+                                       statusCode: 500,
+                                       description: "something went wrong"
+                                   }
+                               });
+                           }
+                           console.log(result);
+                           return res.status(201).json({
+                               success: true,
+                               message: "operation successful",
+                               data: {
+                                   statusCode: 201,
+                                   user: {
+                                       user_id: result.insertId,
+                                       email,
+                                       password,
+                                       token
+                                   }
+                               }
+                           });
+                       });
+                   });
+                }
+            );
         }
-    });
-   });
-
-//    database.query("insert into accounts(acc_name, acc_bank, acc_no) values(?, ?, ?)", [acc_name, acc_bank, acc_no], (error, result, fields) => {
-//        if (error) {
-//            console.log(error);
-//            return res.send(error);
-//        }
-//        console.log(result);
-//    });
-
+    );
 };
